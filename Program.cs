2826d75f6d002f38e81ce2f49771eaf1a4841ddc
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-05-26 11:23:54 -0300

    Removita variável inicial que não estava sendo chamada e adicionado função para opções do menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿// Screen Sound
-string mensagemBoasVindas = "Boas vindas ao Screen Sound";
 
 void ExibirMensagemBoasVindas()
 {
-    Console.WriteLine(@"
-    
+    Console.WriteLine(@"    
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
 ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
 ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
@@ -14,4 +12,14 @@ void ExibirMensagemBoasVindas()
     ");
 }
 
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+
 ExibirMensagemBoasVindas();
+ExibirOpcoesDoMenu();
